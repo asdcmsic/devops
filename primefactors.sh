edb7413549b40565cf1e68410991af1a6a8f472d
Mohammed Omar <asdc.msic@gmail.com>
2023-02-22 21:01:10 +0530

    fix command not found errors

--- primefactors.sh
+++ primefactors.sh
@@ -4,10 +4,10 @@ c=2
 prime=1
 read -p "Enter the number: " num
 
-while [$c -lt num]
+while [ $c -lt $num ]
 do
 	 
- 	if [$[$num%c] -eq 0]
+ 	if [ $[$num%c] -eq 0 ]
 	 then
 		prime=0
 		break
@@ -16,7 +16,7 @@ do
 	c=$[$c+1]
 done
 
-	if [$prime == 0]
+	if [ $prime == 0 ]
 	then
 	echo "$num is not  prime number"
 else
